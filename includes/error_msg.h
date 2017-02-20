b248fabc7379cb7ad6d72dd07a5b12b19eb6add5
Mihai SEINIC <mseinic@student.42.fr>
2017-02-20 17:16:27 +0100

    error_msg

---
+++ includes/error_msg.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_msg.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mseinic <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/20 17:01:36 by mseinic           #+#    #+#             */
+/*   Updated: 2017/02/20 17:16:05 by mseinic          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ERROR_MSG_H
+# define ERROR_MSG_H
+# define NO_PERMISSION_MSG "permission denied"
+# define NO_DIRECTORY_MSG "not a directory"
+# define NO_FILE_OR_DIR_MSG "no such file or directory"
+# define NO_PERMISSION(C, F) (error_msg(C, NO_PERMISSION_MSG, F))
+# define NO_FILE_OR_DIR(C, F) (error_msg(C, NO_FILE_OR_DIR_MSG, F))
+# define NOT_A_DIR(C, F) (error_msg(C, NO_DIRECTORY_MSG, F))
+
+void	error_msg(char *cmd, char *msg, char *file);
+#endif
