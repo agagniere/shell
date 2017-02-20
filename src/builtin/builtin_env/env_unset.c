aa3ef250f8f0b6749db9ea53bc78b31a9032a8ef
Mihai SEINIC <mseinic@e1r3p4.42.fr>
2017-02-20 16:18:05 +0100

    modif

--- src/builtin/builtin_env/env_unset.c
+++ src/builtin/builtin_env/env_unset.c
@@ -6,13 +6,22 @@
 /*   By: mseinic <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/17 17:53:22 by mseinic           #+#    #+#             */
-/*   Updated: 2017/02/17 17:57:32 by mseinic          ###   ########.fr       */
+/*   Updated: 2017/02/20 16:16:59 by mseinic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
-int		env_unset(t_env *env, char *)
+void		env_cell_delete(void	*ptr)
 {
 	t_env_cell	*cell;
+
+	cell = (t_env_cell *)ptr;
+	ft_strdell(&cell->key);
+	ft_strdell(&cell->value);
+}
+
+int		env_unset(t_env *env, t_env_cell *cell)
+{
+	return (0);
 }
