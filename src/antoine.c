b9eb2c801631f1374531095eda82287c2925214c
Xxdzs <sid.xxdzs@gmail.com>
2017-03-02 19:17:00 +0100

    update

--- src/antoine.c
+++ src/antoine.c
@@ -6,13 +6,15 @@
 /*   By: angagnie <angagnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/21 19:45:53 by angagnie          #+#    #+#             */
-/*   Updated: 2017/02/25 16:36:14 by angagnie         ###   ########.fr       */
+/*   Updated: 2017/03/02 18:09:56 by angagnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-** ---------- Operators ----------
-** token  AND_IF    OR_IF    DSEMI
+** ( ) | ; &
+** [ - ]
+** ---------- Multi-char Operators ----------
+0** token  AND_IF    OR_IF    DSEMI
 ** |       '&&'      '||'     ';;'
 ** token  DLESS  DGREAT  LESSAND  GREATAND  LESSGREAT  DLESSDASH
 ** |       '<<'   '>>'    '<&'     '>&'       '<>'       '<<-'
@@ -31,20 +33,20 @@
 **       'in'
 */
 
-#include "automata.h"
+#include "antoine.h"
 
-t_string
-	ft_cleaner(t_is *input)
+int		ft_antoine(t_string *input)
 {
-}
-
-int
-	ft_antoine(t_is *input)
-{
-	t_string	clean;
+	t_sstr		output;
 	t_dfa		atm[1];
 
 	*atm = dfa_new(input);
-	TRANS_ADD(atm, SH_DEFAULT, NEW_TRANS('\\',SH_BACKSLASH));
-	TRANS_ADD(atm, SH_DEFAULT, NEW_TRANS())
+	TRANS_ADD(atm, SH_DEFAULT, NEW_TRANS('_', SH_DEFAULT))
+	TRANS_ADD(atm, SH_DEFAULT, NEW_TRANS('\\', SH_BACKSLASH));
+	TRANS_ADD(atm, SH_DEFAULT, NEW_TRANS('#', SH_COMMENT));
+	TRANS_ADD(atm, SH_DEFAULT, NEW_TRANS('\'', SH_SINGLE_QUOTE));
+	TRANS_ADD(atm, SH_DEFAULT, NEW_TRANS('"', SH_DOUBLE_QUOTE));
+	TRANS_ADD(atm, SH_DEFAULT, NEW_TRANS(' ', SH_GAP));
+	TRANS_ADD(atm, SH_DEFAULT, NEW_TRANS('\t', SH_GAP));
+	TRANS_ADD();
 }
